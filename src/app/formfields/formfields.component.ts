0e5028156508158976fee48d6e1a373ae3304d31
vladimirkaroyan <vladimir.kyan@gmail.com>
2019-12-06 18:43:53 +0400

    Added angular material, generated formfields component

---
+++ src/app/formfields/formfields.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formfields',
+  templateUrl: './formfields.component.html',
+  styleUrls: ['./formfields.component.css']
+})
+export class FormfieldsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
