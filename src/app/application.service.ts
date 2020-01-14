e9ce9d2629b120abbc84405dc6cdbd7003eaf589
vladimirkaroyan <vladimir.kyan@gmail.com>
2020-01-14 14:51:48 +0400

    Add CORS headers to HTTP Requests

--- src/app/application.service.ts
+++ src/app/application.service.ts
@@ -7,20 +7,27 @@ import {globals} from './globals';
 })
 export class ApplicationService {
   hostname = globals.getHostName();
-  protocol = globals.getProtocol() + "//";
+  protocol = globals.getProtocol() + '//';
   getAppFieldsURL: string = this.protocol + this.hostname + '/get-app-fields/';
   // getQuestionsURL: string = this.protocol + this.hostname + '/get-questions/55';
   getQuestionsURL: string = '/assets/quiz-json.json';
+  requestOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET'
+    }
+  };
 
   constructor(private http: HttpClient) {
   }
 
   getQuestions() {
-    return this.http.get(this.getQuestionsURL);
+    return this.http.get(this.getQuestionsURL, this.requestOptions);
   }
 
   getAppFields(appId: any, eventId: any) {
-    return this.http.get(this.getAppFieldsURL + appId + '?eventId=' + eventId + '');
+    return this.http.get(this.getAppFieldsURL + appId + '?eventId=' + eventId + '', this.requestOptions);
     // For Local use function below
     // return this.http.get(this.getAppFieldsURL);
   }
